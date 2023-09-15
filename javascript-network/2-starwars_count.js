2f17525d2e5014270f28f5b6187aa94bc57eaeff
SundayEfeTerry <info.techbro.set@gmail.com>
2023-09-15 03:54:36 -0700

    --task--

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -6,12 +6,22 @@ const request = require('request');
 
 // pass cmd line argument
 const url = process.argv[2];
+const parts = [];
 
 // make a GET request
-request
-.get(url+18, {encoding: 'utf-8'})
-.on('data', function (data) {
-    response = JSON.parse(data);
-    const count_ = response.films.length;
-    console.log(count_);
+request.get(url, {encoding: 'utf-8'})
+.on('data', (data) => {
+    parts.push(data);
+})
+.on('complete', () => {
+    const resp = JSON.parse(parts);
+    let num_films = 0;
+    resp.results.forEach(function (actor) {
+       actor.characters.forEach((act) => {
+        if (act.includes('https://swapi-api.alx-tools.com/api/people/18')){
+            num_films++;
+        }
+       })
+    }) 
+    console.log(num_films);
 })
